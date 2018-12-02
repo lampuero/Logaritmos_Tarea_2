4680c81be58f49c1f4366398f40034c20ed1be20
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-12-02 19:13:37 -0300

    Dijistra con cola de prioridad.

--- src/Other/DijkstraWithPriorityQueue.java
+++ src/Other/DijkstraWithPriorityQueue.java
@@ -1,10 +1,37 @@
 package Other;
 
-public abstract class DijkstraWithPriorityQueue {
-    static MyPriorityQueue queue;
-    static int[] dist;
-    static Integer[] prev;
+import java.util.ArrayList;
 
-    private void initialize(){}
-    
+public abstract class DijkstraWithPriorityQueue {
+    MyPriorityQueue queue;
+    int[] dist;
+    Integer[] prev;
+
+
+    public ResultDijkstra algorithmDijkstra(int origen, ArrayList<Pair>[] grafo){
+        int n = grafo.length;
+        for (int i = 0; i < n; i++){
+            if (i == origen){
+                dist[i] = 0;
+            }
+            else {
+                dist[i] = Integer.MAX_VALUE;
+            }
+            prev[i] = null;
+            queue.insert(i, dist[i]);
+        }
+
+        while (!queue.isEmpty()){
+            int m = queue.extractMinimum();
+            for (Pair pair: grafo[m]){
+                int newpriority = dist[m] + pair.getDistance();
+                if (newpriority < dist[pair.getVertice()]){
+                    dist[pair.getVertice()] = newpriority;
+                    prev[pair.getVertice()] = m;
+                    queue.decreaseKey(pair.getVertice(), newpriority);
+                }
+            }
+        }
+        return new ResultDijkstra(dist, prev);
+    }
 }
