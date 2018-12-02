9fee007dce1fc37365a800943a2b8538dd0539be
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-12-02 19:12:21 -0300

    Clase abstracta de cola de prioridad con las firmas de las funciones.

--- src/Other/MyPriorityQueue.java
+++ src/Other/MyPriorityQueue.java
@@ -1,4 +1,12 @@
 package Other;
 
 public abstract class MyPriorityQueue {
+
+    public abstract void insert(int vertice, int priority);
+
+    public abstract void decreaseKey(int vertice, int newPriority);
+
+    public abstract int extractMinimum();
+
+    public abstract boolean isEmpty();
 }
