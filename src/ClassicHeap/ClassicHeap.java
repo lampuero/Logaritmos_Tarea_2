2ed3f9ca14bf894e767ca7fa5182355f6a4df526
lampuero <luis.ampuero@ug.uchile.cl>
2018-12-04 13:42:58 -0300

    Implementado algoritmo con heap clasico.
    Signed-off-by: lampuero <luis.ampuero@ug.uchile.cl>

--- src/ClassicHeap/ClassicHeap.java
+++ src/ClassicHeap/ClassicHeap.java
@@ -1,8 +1,40 @@
 package ClassicHeap;
 
+import Other.Pair;
+import Other.ResultDijkstra;
+
+import java.util.ArrayList;
+
 public class ClassicHeap {
 
-    PriorityQueueWithHeap queueWithHeap;
+    private static int[] dist;
+    private static Integer[] prev;
 
+    public static ResultDijkstra algorithmDijkstra(int origen, ArrayList<Pair>[] grafo){
+        PriorityQueueWithHeap queue = new PriorityQueueWithHeap();
+        int n = grafo.length;
+        for (int i = 0; i < n; i++){
+            if (i == origen){
+                dist[i] = 0;
+            }
+            else {
+                dist[i] = Integer.MAX_VALUE;
+            }
+            prev[i] = null;
+            queue.insert(i, dist[i]);
+        }
 
+        while (!queue.isEmpty()){
+            int m = queue.extractMinimum();
+            for (Pair pair: grafo[m]){
+                int newpriority = dist[m] + pair.getDistance();
+                if (newpriority < dist[pair.getVertice()]){
+                    dist[pair.getVertice()] = newpriority;
+                    prev[pair.getVertice()] = m;
+                    queue.decreaseKey(pair.getVertice(), newpriority);
+                }
+            }
+        }
+        return new ResultDijkstra(dist, prev);
+    }
 }
