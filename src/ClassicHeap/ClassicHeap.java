0e6de5ad14bfece8a2e60ade463c5b5e082f39ef
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-12-09 01:31:00 -0300

    Anula ultimo commit.

--- src/ClassicHeap/ClassicHeap.java
+++ src/ClassicHeap/ClassicHeap.java
@@ -9,10 +9,9 @@ public class ClassicHeap {
 
     public static ResultDijkstra algorithmDijkstra(int origen, ArrayList<Pair>[] grafo){
         int n = grafo.length;
-        Element[] elements = new Element[n];
         int[] dist = new int[n];
         Integer[] prev = new Integer[n];
-        PriorityQueueWithHeap queue = new PriorityQueueWithHeap();
+        PriorityQueueWithHeap queue = new PriorityQueueWithHeap(n);
 
         for (int i = 0; i < n; i++){
             if (i == origen){
@@ -22,18 +21,17 @@ public class ClassicHeap {
                 dist[i] = Integer.MAX_VALUE;
             }
             prev[i] = null;
-            elements[i] = new Element(i, dist[i]);
-            queue.insert(elements[i]);
+            queue.insert(i, dist[i]);
         }
 
         while (!queue.isEmpty()){
             int m = queue.extractMinimum();
             for (Pair pair: grafo[m]){
-                int newPriority = dist[m] + pair.distance;
-                if (newPriority < dist[pair.vertice]){
-                    dist[pair.vertice] = newPriority;
+                int newpriority = dist[m] + pair.distance;
+                if (newpriority < dist[pair.vertice]){
+                    dist[pair.vertice] = newpriority;
                     prev[pair.vertice] = m;
-                    queue.decreaseKey(elements[pair.vertice], newPriority);
+                    queue.decreaseKey(pair.vertice, newpriority);
                 }
             }
         }
